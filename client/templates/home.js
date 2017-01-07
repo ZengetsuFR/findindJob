c1ac57c057af17aeecc184e5973bd994661090b6
Joel Bragance <j.bragance@lekiosque.fr>
2017-01-07 15:51:40 +0100

    working progress facebook

--- client/templates/home.js
+++ client/templates/home.js
@@ -97,7 +97,7 @@ Template.job.events({
                 title: 'Ton futur métier est-il porteur ?',
                 description: sharingElementToFacebook.texttoShare ,
                 link: sharingElementToFacebook.facebookurl,
-                href:sharingElementToFacebook.facebookurl
+                href:"www.queldebouche.fr"
             }, function (response) { });
     }
 })
